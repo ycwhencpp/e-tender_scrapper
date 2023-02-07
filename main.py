ab6032897c881e90f03aba024403bf0dfd9a36ce
Anurag choudhary <anurag.jay1002@gmail.com>
2023-02-07 16:28:15 +0530

    Scrapper (#2)
    * added csv file
    
    * added metadata and changed the format of writing it in csv file

--- main.py
+++ main.py
@@ -1,36 +1,72 @@
-import os
 import requests
 import csv
 from bs4 import BeautifulSoup
-import logging
-import logging.config
+from datetime import datetime
 
 class scraper:
     def __init__(self, url):
         self.url = url
         self.tenders = []
+        self.metadata = {
+            'data_source': 'Central Public Procurement portal',
+            'data_collection_date': datetime.now().strftime("%Y-%m-%d"),
+            'url': url,
+            'other_metadata': 'Any additional information about the data'
+        }
         
     def get_data(self):
+
+        # getting whole html content 
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content,'html.parser')
-        tender_table=soup.find('table',attrs={'class':'list_table'})
-        # print(tender_table)
+
+
+        tender_table=soup.find('table',attrs={'class':'list_table'}) # scraping and storing tables whose class name is"list_table"
+
         if tender_table:
-            rows=tender_table.find_all('tr')
+
+            rows=tender_table.find_all('tr') # fetching the rows from the table
+
             for row in rows:
-                if 'list_header' in row['class']:
+
+                if 'list_header' in row['class']: # if it is the header row just skip it 
                     continue
-                cols = row.find_all('td')
-                cols =[col.text.strip() for col in cols]
-                self.tenders.append(cols)
+
+                cells = row.find_all('td') #fetching the table data
+
+                if len(cells) > 0:
+
+                    #storing the relevant info in tender dict
+                    tender = {
+                        'S.no': cells[0].text.strip(),
+                        'tender_published_Date': cells[1].text.strip(),
+                        'tender_closing_date': cells[2].text.strip(),
+                        'tender_opening_date': cells[3].text.strip(),
+                        'tender_tile_and_id': cells[4].text.strip(),
+                        'tender_origanisation':cells[5].text.strip(),
+                }
+                self.tenders.append(tender)
         else:
             print("unable to find the tenders table")
-
+            return
+    
+    # to write the fetched data into csv file
     def write_csv(self):
-        with open('etenders.csv','w',newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['S.no','E-published Date','Closing Date' , 'Opening Date','Title and Ref.No./Tender ID','Organisation Chain'])
-            writer.writerows(self.tenders)
+
+        with open('check.csv','w',newline='') as file:
+
+            # intailising the field names
+            fieldnames = ['data_source', 'data_collection_date', 'url', 'other_metadata',
+                          'S.no','tender_published_Date','tender_closing_date' , 
+                          'tender_opening_date','tender_tile_and_id',
+                          'tender_origanisation']
+
+            
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            for tender in self.tenders:
+                tender.update(self.metadata)
+                writer.writerow(tender)
 
 if __name__ == '__main__':
     scraper = scraper('https://etenders.gov.in/eprocure/app?component=%24DirectLink&page=FrontEndTendersByOrganisation&service=direct')
