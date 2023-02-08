df5db9581bf3ecf9c1098bb9385d9dc33a6bfed8
Anurag choudhary <anurag.jay1002@gmail.com>
2023-02-08 21:22:33 +0530

    Optimize (#8)
    * added csv file
    
    * added metadata and changed the format of writing it in csv file
    
    * improved performance
    
    * added unit_tests and logging info
    
    * added unit_tests and optimized code
    
    * final build/ refactored code
    
    * added config.py for config of params

---
+++ config.py
@@ -0,0 +1,7 @@
+import logging
+
+URL = 'https://etenders.gov.in/eprocure/app?component=%24DirectLink&page=FrontEndTendersByOrganisation&service=direct'
+LOG_FILE = 'scraper.log'
+FILE_PATH = 'e_tender_data.csv'
+LOG_LEVEL = logging.INFO
+
